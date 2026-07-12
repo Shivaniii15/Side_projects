83bb1731e00f634a6bf3a4b5b983dc0d360ab8bb
Shivaniii15 <105461293+Shivaniii15@users.noreply.github.com>
2026-07-12 22:49:58 +1200

    added error handling

--- web-scrapping/scraper..py
+++ web-scrapping/scraper..py
@@ -4,7 +4,14 @@ from bs4 import BeautifulSoup
 
 url = "https://books.toscrape.com/catalogue/category/books/music_14/index.html"
 
-response = requests.get(url)
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+except requests.exceptions.RequestException as e:
+    logging.error(f"Request failed: {e}")
+    print(f"Request failed: {e}")
+    exit()
+
 soup = BeautifulSoup(response.text, "html.parser")
 
 logging.basicConfig(
@@ -17,12 +24,12 @@ books = soup.find_all("article", class_="product_pod")
 
 for book in books:
     title = book.h3.a["title"]
-    price = book.find("p", class_="price_color").text
+    price_text = book.find("p", class_="price_color").text
+    price = float(price_text.replace("£", "").replace("Â", ""))
 
     logging.info(f"Title: {title}, Price: {price}")
     print(f"Title: {title}, Price: {price}")
 
-books = soup.find_all("article", class_="product_pod")
 
 logging.info(f"Status Code: {response.status_code}")
 logging.info(f"Number of books found: {len(books)}")
